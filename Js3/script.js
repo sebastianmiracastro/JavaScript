5d742a0a8efa76e4923f06897b5bd59d06143b18
sebastianmiracastro <smira44@misena.edu.co>
2021-10-20 17:46:11 -0500

    Trabajo autonomo dia 20/10/2021

--- Js3/script.js
+++ Js3/script.js
@@ -1,7 +1,7 @@
 const button = document.getElementById("button");
 const body = document.querySelector("body");
-const colorNumber = document.querySelector(".color-number");
-const colorCard = document.querySelector(".color-card");
+const colorNumber = document.querySelector("#color-number");
+const Tittle = document.querySelector("#title")
 
 const generateRandomColor = () => {
   const r = Math.floor(Math.random() * 256);
@@ -14,9 +14,11 @@ const generateRandomColor = () => {
 
 const setBackgrount = () => {
   const newColor = generateRandomColor();
-  console.log(newColor);
   colorNumber.innerHTML = newColor;
+  colorNumber.style.color= newColor;
   body.style.backgroundColor = newColor;
+  button.style.background = newColor;
+  Tittle.style.color = newColor;
 };
 
 button.addEventListener("click", setBackgrount);
---
+++ Js4/script.js
@@ -0,0 +1,1 @@
+const parrafo = document.createElement("p")
