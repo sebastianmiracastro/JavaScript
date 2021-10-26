511431c00070981c167bc124837afd3e666e6254
sebastianmiracastro <smira44@misena.edu.co>
2021-10-25 21:27:08 -0500

    Terminando Funcionamiento Del Script Del Java Script

--- Js4/script.js
+++ Js4/script.js
@@ -8,3 +8,19 @@ Button.addEventListener('click', SeeName);
 function SeeName(){
     PrintedNameUser.textContent=NameUser.value.repeat(1);
 }
+
+const Signs = NameUser.value.includes('+', '-', '*', '/', '_', ',', '.', ':', '?', '¿', '<', '>', '=');
+
+NameUser.addEventListener('keyup', function(){
+
+    if (NameUser.value.includes('+') || NameUser.value.includes('-') || NameUser.value.includes('/') || NameUser.value.includes('*') || NameUser.value.includes('_')  || NameUser.value.includes(':') || NameUser.value.includes('.') || NameUser.value.includes(',')  || NameUser.value.includes('?') || NameUser.value.includes('¿') || NameUser.value.includes('<') || NameUser.value.includes('>') || NameUser.value.includes('=')){
+        Validate.textContent = 'No Se Admite Signos: +, -, /, *, _.';
+        Validate.style.color = 'red';
+        Button.disabled = true;
+    }
+    else{
+        Validate.textContent = 'Usuario Valido'; 
+        Validate.style.color = 'green';
+        Button.disabled = false
+    }
+})
