3f2878809bec39a748083fb6db24b59c11379f74
sebastianmiracastro <smira44@misena.edu.co>
2021-10-25 21:22:57 -0500

    Funcionamiento Del Boton

--- Js4/script.js
+++ Js4/script.js
@@ -2,3 +2,9 @@ const NameUser = document.querySelector('#Name');
 const PrintedNameUser = document.querySelector('#PrintedNameUser');
 const Button = document.querySelector('#Button');
 const Validate = document.querySelector('#Validate');
+
+Button.addEventListener('click', SeeName);
+
+function SeeName(){
+    PrintedNameUser.textContent=NameUser.value.repeat(1);
+}
