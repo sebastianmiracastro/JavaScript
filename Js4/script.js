6bb1b930d7355ec161f7b0b4c851182cf1b6c976
sebastianmiracastro <smira44@misena.edu.co>
2021-10-25 21:19:57 -0500

    Trayendo los elmentos del html

---
+++ Js4/script.js
@@ -0,0 +1,4 @@
+const NameUser = document.querySelector('#Name');
+const PrintedNameUser = document.querySelector('#PrintedNameUser');
+const Button = document.querySelector('#Button');
+const Validate = document.querySelector('#Validate');
