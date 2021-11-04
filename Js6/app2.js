1a15ff29f1aa4f6064dfb098912297dc5b73f718
sebastianmiracastro <smira44@misena.edu.co>
2021-11-04 01:09:28 -0500

    Cambios y dudas diarias

--- Js6/app2.js
+++ Js6/app2.js
@@ -1,43 +1,119 @@
-const main2 = document.getElementById('main2');
+const addToShoppingCartButtons = document.querySelectorAll('#btn-card');
+addToShoppingCartButtons.forEach((addToCartButton) => {
+  addToCartButton.addEventListener('click', addToCartClicked);
+});
 
-const Shop2 = (name2, src2, price2) => {
-    const card_render2 = document.createElement('div');
-    const header_card2 = document.createElement('div');
-    const h2_title2 = document.createElement('h2');
-    const card_main2 = document.createElement('div');
-    const div_image2 = document.createElement('div');
-    const image_card2 = document.createElement('img');
-    const btn_card2 = document.createElement('button');
-    const price_p2 = document.createElement('p');
+const comprarButton = document.querySelector('.comprarButton');
+comprarButton.addEventListener('click', comprarButtonClicked);
 
-    card_render2.classList.add('card2');
-    header_card2.classList.add('header-card2');
-    h2_title2.setAttribute('id', 'h2-card2');
-    h2_title2.textContent = name2;
-    card_main2.classList.add('card-main2')
-    div_image2.classList.add('card-image2')
-    image_card2.setAttribute('src', src2);
-    image_card2.setAttribute('alt', 'Image Card2');
-    image_card2.classList.add('img2');
-    price_p2.classList.add('price_p2');
-    price_p2.textContent = price2;
+const shoppingCartItemsContainer = document.querySelector(
+  '.shoppingCartItemsContainer'
+);
 
-    btn_card2.setAttribute('id', 'btn-card2');
-    btn_card2.textContent = 'Order. . .';
-
-    div_image2.appendChild(image_card2);
-    header_card2.appendChild(h2_title2);
-    card_render2.appendChild(header_card2);
-    card_render2.appendChild(card_main2);
-    card_render2.appendChild(div_image2);
-    card_render2.appendChild(price_p2);
-    card_render2.appendChild(btn_card2);
-    main2.appendChild(card_render2);
+function addToCartClicked(event) {
+    const button = event.target;
+    const item = button.closest('.item');
+  
+    const itemTitle = item.querySelector('.item-title').textContent;
+    const itemPrice = item.querySelector('.item-price').textContent;
+    const itemImage = item.querySelector('.item-image').src;
+  
+    addItemToShoppingCart(itemTitle, itemPrice, itemImage);
 }
 
-function create_cards2() {
-    products2.forEach(Shopping2 => {
-        Shop2(Shopping2.name2, Shopping2.src2, Shopping2.price2)
-        console.log(products2);
-    })
-}
+function addItemToShoppingCart(itemTitle, itemPrice, itemImage) {
+    const elementsTitle = shoppingCartItemsContainer.getElementsByClassName(
+      'shoppingCartItemTitle'
+);
+for (let i = 0; i < elementsTitle.length; i++) {
+    if (elementsTitle[i].innerText === itemTitle) {
+    let elementQuantity = elementsTitle[
+        i
+        ].parentElement.parentElement.parentElement.querySelector(
+          '.shoppingCartItemQuantity'
+        );
+        elementQuantity.value++;
+        $('.toast').toast('show');
+        updateShoppingCartTotal();
+        return;
+      }
+    }
+  
+    const shoppingCartRow = document.createElement('div');
+    const shoppingCartContent = `
+    <div class="row shoppingCartItem">
+          <div class="col-6">
+              <div class="shopping-cart-item d-flex align-items-center h-100 border-bottom pb-2 pt-3">
+                  <img src=${itemImage} class="shopping-cart-image">
+                  <h6 class="shopping-cart-item-title shoppingCartItemTitle text-truncate ml-3 mb-0">${itemTitle}</h6>
+              </div>
+          </div>
+          <div class="col-2">
+              <div class="shopping-cart-price d-flex align-items-center h-100 border-bottom pb-2 pt-3">
+                  <p class="item-price mb-0 shoppingCartItemPrice">${itemPrice}</p>
+              </div>
+          </div>
+          <div class="col-4">
+              <div
+                  class="shopping-cart-quantity d-flex justify-content-between align-items-center h-100 border-bottom pb-2 pt-3">
+                  <input class="shopping-cart-quantity-input shoppingCartItemQuantity" type="number"
+                      value="1">
+                  <button class="btn btn-danger buttonDelete" type="button">X</button>
+              </div>
+          </div>
+      </div>`;
+    shoppingCartRow.innerHTML = shoppingCartContent;
+    shoppingCartItemsContainer.append(shoppingCartRow);
+  
+    shoppingCartRow
+      .querySelector('.buttonDelete')
+      .addEventListener('click', removeShoppingCartItem);
+  
+    shoppingCartRow
+      .querySelector('.shoppingCartItemQuantity')
+      .addEventListener('change', quantityChanged);
+  
+    updateShoppingCartTotal();
+  }
+  
+  function updateShoppingCartTotal() {
+    let total = 0;
+    const shoppingCartTotal = document.querySelector('.shoppingCartTotal');
+  
+    const shoppingCartItems = document.querySelectorAll('.shoppingCartItem');
+  
+    shoppingCartItems.forEach((shoppingCartItem) => {
+      const shoppingCartItemPriceElement = shoppingCartItem.querySelector(
+        '.shoppingCartItemPrice'
+      );
+      const shoppingCartItemPrice = Number(
+        shoppingCartItemPriceElement.textContent.replace('€', '')
+      );
+      const shoppingCartItemQuantityElement = shoppingCartItem.querySelector(
+        '.shoppingCartItemQuantity'
+      );
+      const shoppingCartItemQuantity = Number(
+        shoppingCartItemQuantityElement.value
+      );
+      total = total + shoppingCartItemPrice * shoppingCartItemQuantity;
+    });
+    shoppingCartTotal.innerHTML = `${total.toFixed(2)}€`;
+  }
+  
+  function removeShoppingCartItem(event) {
+    const buttonClicked = event.target;
+    buttonClicked.closest('.shoppingCartItem').remove();
+    updateShoppingCartTotal();
+  }
+  
+  function quantityChanged(event) {
+    const input = event.target;
+    input.value <= 0 ? (input.value = 1) : null;
+    updateShoppingCartTotal();
+  }
+  
+  function comprarButtonClicked() {
+    shoppingCartItemsContainer.innerHTML = '';
+    updateShoppingCartTotal();
+  }
+  
--- Js6/products2.js
+++
@@ -1,6 +0,0 @@
-const products2 = [
-    {id2:1,name2:'A Dos Metros De Ti',src2:'Images/De Amor/ADosMetrosDeTi.jpg',price2:'$30.000',},
-    {id2:2,name2:'Del Amor Y Otros Demonios',src2:'Images/De Amor/DelAmorYOtrosDemonios.jpg',price2:'$15.000',},
-    {id2:4,name2:'Entre Dos Universos',src2:'Images/De Amor/EntreDosUniversos.jpg',price2:'$9.000',},
-    {id2:5,name2:'Don Quijote De La Mancha',src2:'Images/De Amor/FuimosCancion.jpg',price2:'$16.000',},
-]
