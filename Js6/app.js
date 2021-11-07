057bccc2e011223a0dfe1ba7fe5d853b7bd10016
sebastianmiracastro <smira44@misena.edu.co>
2021-11-07 18:31:06 -0500

    Cambios totales En El Documento

--- Js6/app.js
+++ Js6/app.js
@@ -1,106 +1,142 @@
 const main = document.getElementById('main');
+const container_productos = document.querySelector('.container_products');
 
-const Shop = (name,src, price) => { 
-  const card_render = document.createElement('div');
-  const header_card = document.createElement('div');
-  const h2_title = document.createElement('h2');
-  const card_main = document.createElement('div');
-  const div_image = document.createElement('div');
-  const image_card = document.createElement('img');
-  const btn_card = document.createElement('button');
-  const price_p = document.createElement('p');
-  
-  card_render.classList.add('card');
-  header_card.classList.add('header-card');
-  h2_title.setAttribute('id', 'h2-card1');
-  h2_title.textContent = name;
-  card_main.classList.add('card-main');
-  div_image.classList.add('card-image');
-  image_card.setAttribute('src',src); 
-  image_card.setAttribute('alt','Image Card');
-  image_card.classList.add('img'); 
-  price_p.classList.add('price_p');
-  price_p.textContent = price
-  
-  btn_card.setAttribute('id','btn-card');
-  btn_card.textContent = 'Order. . .';
-  btn_card.addEventListener('click', AddProducts)
-  
-  div_image.appendChild(image_card);
-  header_card.appendChild(h2_title);
-  card_render.appendChild(header_card);
-  card_render.appendChild(card_main);
-  card_render.appendChild(div_image);
-  card_render.appendChild(price_p)
-  card_render.appendChild(btn_card);
-  main.appendChild(card_render);
+ListShoppingCart = [];
+
+create_cards(); 
+function create_cards(){
+  products.forEach(Books => {
+    const card_render = document.createElement('div');
+    const header_card = document.createElement('div');
+    const h2_title = document.createElement('h2');
+    const card_main = document.createElement('div');
+    const div_image = document.createElement('div');
+    const image_card = document.createElement('img');
+    const btn_card = document.createElement('button');
+    const parrafo = document.createElement('div')
+    const h5_title = document.createElement('h5');
+
+    card_render.classList.add('card');
+    header_card.classList.add('header-card');
+    h2_title.setAttribute('id', 'h2');
+    h2_title.textContent = Books.name;
+    card_main.classList.add('card-main');
+    div_image.classList.add('card-image');
+    image_card.setAttribute('src',Books.src); 
+    image_card.setAttribute('alt','Image Card');
+    image_card.classList.add('img'); 
+    header_card.classList.add('parrafo');
+    h5_title.setAttribute('id', 'h5');
+    h5_title.textContent = Books.price;
+
+    btn_card.setAttribute('id', Books.id);
+    btn_card.textContent = 'Order...';
+    
+    btn_card.addEventListener('click', AddToCart);
+    
+    div_image.appendChild(image_card);
+    header_card.appendChild(h2_title);
+    card_render.appendChild(header_card);
+    card_render.appendChild(card_main);
+    card_render.appendChild(div_image);
+    parrafo.appendChild(h5_title);
+    card_render.appendChild(parrafo);
+    card_render.appendChild(btn_card);
+    main.appendChild(card_render);
+  });
 }
-function create_cards() {
-  products.forEach(Shopping => {
-    Shop(Shopping.name, Shopping.src, Shopping.price)
-    console.log(products);
+function SeeCart (){
+
+  container_productos.innerHTML = '';
+  const indexUnico = new Set(ListShoppingCart)
+  indexUnico.forEach((element) => {
+    const BooksAtributes = products.filter((item) => {
+      return parseInt(element) === item.id
+    })
+
+    let cont = 0;
+
+    for(let id of ListShoppingCart) {
+        if(id === element) {
+            cont++;
+        }
+    }
+    const Container_Books = document.createElement('div')
+
+    const nombre = document.createElement('p');
+    const precio = document.createElement('p');
+    const contador = document.createElement('p');
+    const btn_sum = document.createElement('button');
+    const btn_substract = document.createElement('button');
+    const btn_eliminate = document.createElement('button');
+    btn_sum.setAttribute('id', BooksAtributes[0].id);
+    btn_substract.setAttribute('id',BooksAtributes[0].id);
+    btn_eliminate.setAttribute('id',BooksAtributes[0].id);
+
+    nombre.textContent = BooksAtributes[0].name;
+    precio.textContent = BooksAtributes[0].price;
+    btn_sum.textContent = '+';
+    btn_substract.textContent = '-'
+    btn_eliminate.textContent = 'X';
+    contador.textContent = cont;
+    
+    
+    Container_Books.appendChild(nombre);
+    Container_Books.appendChild(precio);
+    Container_Books.appendChild(contador)
+    Container_Books.appendChild(btn_sum);
+    Container_Books.appendChild(btn_substract);
+    Container_Books.appendChild(btn_eliminate)
+
+    btn_sum.addEventListener('click', AddToCart);
+    btn_substract.addEventListener('click', SubstractProduct);
+    btn_eliminate.addEventListener('click', EliminateProduct)
+
+    btn_sum.style.marginTop = '1.5em';
+    btn_sum.style.width = '4%';
+    btn_sum.style.height = '2.5rem';
+    btn_sum.style.borderRadius = '14px';
+    btn_sum.style.backgroundColor = 'rgb(243, 138, 165)';
+
+    btn_substract.style.marginTop = '1.5em';
+    btn_substract.style.width = '4%';
+    btn_substract.style.height = '2.5rem';
+    btn_substract.style.borderRadius = '14px';
+    btn_substract.style.backgroundColor = 'rgb(243, 138, 165)';
+
+    btn_eliminate.style.marginTop = '1.5em';
+    btn_eliminate.style.width = '4%';
+    btn_eliminate.style.height = '2.5rem';
+    btn_eliminate.style.borderRadius = '14px';
+    btn_eliminate.style.backgroundColor = 'rgb(243, 138, 165)';
+
+    Container_Books.style.padding = '10px';
+    Container_Books.style.margin = '5px 0';
+    Container_Books.style.border = '1px solid #d42853';
+    Container_Books.style.boxShadow= '0 0 20px #d42853';
+    Container_Books.style.display = 'flex';
+    Container_Books.style.justifyContent = 'space-between';
+    Container_Books.style.alignItems = 'stretch';
+    Container_Books.style.borderRadius = '10px';
+    
+    container_productos.appendChild(Container_Books);
   })
 }
-create_cards();
-
-/* Shopping Cart */
-
-const Container_Products = document.getElementById('main');
-const Btn_ShoppingCart = document.querySelector('.btn-marketcart')
-const ShppingCart = document.querySelector('.Shopping-cart ocult')
-
-ListSelectShoppingCart = []
-
-const SeeShoppingCart = () =>{
-    let  List = [... new Set(ListSelectShoppingCart)];
-
-    List.forEach(item => {
-        const All_Products = products.filter(ElementsProducts => {
-            return ElementsProducts.id === parseInt(item)
-        })
-        let counter = 0;
-
-        for(let id of ListSelectShoppingCart){
-            if(id === item) {
-                counter++;
-            }
-        }
-
-        const CardProductCart = document.createElement('div')
-        const Name = document.createElement('p');
-        const Price = document.createElement('p')
-        const Counters = document.createElement('p')
-        const BTN_Sum = document.createElement('button')
-        const BTN_Subtract = document.createElement('button')
-        const BTN_Eliminate = document.createElement('button')
-        
-        BTN_Sum.setAttribute('id_product', All_Products[0].id);
-        BTN_Subtract.setAttribute('id_product', All_Products[0].id);
-        BTN_Eliminate.setAttribute('id_product', All_Products[0].id);
-
-        Name.textContent = All_Products[0].name;
-        Price.textContent = All_Products[0].price;
-        BTN_Sum.textContent = '+';
-        BTN_Subtract = '-';
-        BTN_Eliminate.textContent = 'X';
-        Counters.textContent = counter;
-
-        CardProductCart.classList.add('card_render')
-        CardProductCart.appendChild(Name);
-        CardProductCart.appendChild(Price)
-        CardProductCart.appendChild(Counters)
-        CardProductCart.appendChild(BTN_Sum)
-        CardProductCart.appendChild(BTN_Subtract)
-        CardProductCart.appendChild(BTN_Eliminate)
-
-        BTN_Sum.addEventListener('click', AddProducts)
-        BTN_Subtract.addEventListener('click', SubtractProducts)
-        BTN_Eliminate.addEventListener('click', EliminateProducts)
-        ShppingCart.appendChild(CardProductCart)
-    })
+function AddToCart(e){
+  ListShoppingCart.push(e.target.getAttribute('id'));
+  SeeCart();
 }
 
-function AddProducts(e) {
-    ListSelectShoppingCart.push(e.target.getAttribute('id'));
-    SeeShoppingCart();
+const SubstractProduct = (e) => {
+  let item = e.target.getAttribute('id') 
+  ListShoppingCart.splice(parseInt(ListShoppingCart.indexOf(item)),1)
+  SeeCart();
+}
+
+const EliminateProduct = (e) => {
+  let item = e.target.getAttribute('id');
+  ListShoppingCart = ListShoppingCart.filter((id_cats) => {
+      return id_cats !== item;
+  });
+  SeeCart();
 }
