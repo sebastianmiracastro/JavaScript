a4c0d0e32ee0dc1fe29841851884430a7abd372a
sebastianmiracastro <smira44@misena.edu.co>
2021-11-10 18:03:40 -0500

    Consume la api, crear funcion y añadir los valores ISSUES 9

--- Js9/app.js
+++ Js9/app.js
@@ -5,13 +5,21 @@ function Fetch_Characters() {
     fetch(URLCharacter)
     .then(response => response.json())
     .then(data => {
-
-
-
-
-
-           
+        const container = document.createElement('div')
+        const message = document.createElement('p')
+        const characters = document.createElement('p')
+        const img = document.createElement('img')
+        const position = document.createElement('p')
+        
+        container.appendChild(message)
+        container.appendChild(characters)
+        container.appendChild(img)
+        container.appendChild(position)
     })
 }
 
 Btn_Change.addEventListener('click', Fetch_Characters)
+
+const data = [
+]
+
