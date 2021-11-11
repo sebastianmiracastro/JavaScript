5518f778adc7237b669e3a333ececeb0e5c97d60
sebastianmiracastro <smira44@misena.edu.co>
2021-11-11 10:33:45 -0500

    Taller finalizado

--- Js9/app.js
+++ Js9/app.js
@@ -1,21 +1,16 @@
 const URLCharacter = 'https://thesimpsonsquoteapi.glitch.me/quotes'
 const Btn_Change = document.getElementById('Change')
+const Tittle = document.getElementById('h2')
+const Txt = document.getElementById('h5')
+const ImgCard = document.querySelector('.Img')
+const Direction = document.querySelector('#Direction')
 
 function Fetch_Characters() {
     fetch(URLCharacter)
     .then(response => response.json())
     .then(data => {
-        const container = document.createElement('div')
-        const message = document.createElement('p')
-        const characters = document.createElement('p')
-        const img = document.createElement('img')
-        const position = document.createElement('p')
-        
-        container.appendChild(message)
-        container.appendChild(characters)
-        container.appendChild(img)
-        container.appendChild(position)
-    })
+        createCar(data)
+    }).catch(error => console.log(error))
 }
 
 Btn_Change.addEventListener('click', Fetch_Characters)
@@ -23,3 +18,11 @@ Btn_Change.addEventListener('click', Fetch_Characters)
 const data = [
 ]
 
+Fetch_Characters();
+
+function createCar(data) {
+    Tittle.textContent = data[0].character;
+    Txt.textContent = data[0].quote;
+    ImgCard.setAttribute('src', data[0].image)
+    Direction.textContent = data[0].characterDirection;
+}
