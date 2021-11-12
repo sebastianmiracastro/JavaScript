10b960e4ae6ec5fbe4f80bae16d69ff267fea2be
sebastianmiracastro <smira44@misena.edu.co>
2021-11-12 17:45:08 -0500

    Errores que me quieren matar

--- Js10/app.js
+++ Js10/app.js
@@ -1,21 +1,36 @@
-// const URLCharacterEvilMory =  'https://rickandmortyapi.com/api/character/118'
-// const URLCharacterRandyDicknose = 'https://rickandmortyapi.com/api/character/275'
-// const URLCharacterRatBoss = 'https://rickandmortyapi.com/api/character/276'
-// const URLCharacterDoubleMicrowawe = 'https://rickandmortyapi.com/api/character/550'
-// const URLCharacterTooCuteToMurder = 'https://rickandmortyapi.com/api/character/715'
-// const URlCharacterGobo = 'https://rickandmortyapi.com/api/character/147'
-// const URLCharacterDuckWithMuscles = 'https://rickandmortyapi.com/api/character/109'
 const URLAll = 'https://rickandmortyapi.com/api/character/'
 
 const Name = document.getElementById('Name')
 const Img = document.getElementById('Img')
-
+const SelectionCharacter = document.getElementById('SelectionCharacter')
+const Options = document.getElementById('Options') 
+const SexType = document.getElementById('SexType')
+const container = document.getElementById('container')
 
 function Fetch_characte() {
     fetch(URLAll)
     .then(response => response.json())
-    .then(data => {
-        create_cards(data)
-    }).catch(error => console.log(error))
+    .then(data => console.log(data.results[0].name, data.results[0].gender, data.results[0].image))
 }
+const data = []
+ 
+Fetch_characte();
 
+
+function Create_Card(data) {
+    const cards1 = document.createElement('div');
+    const CardsOption = document.createElement('select')
+    const name_card = document.createElement('option');
+    const image_card = document.createElement('img');
+    const gender_card = document.createElement('p');
+
+    name_card.textContent = data.results[0].name;
+    gender_card.textContent = data.results[0].gender;
+    image_card.setAttribute('src', data.results[0].image);
+
+    cards1.appendChild(CardsOption)
+    cards1.appendChild(name_card)
+    cards1.appendChild(gender_card)
+    cards1.appendChild(image_card)
+    container.appendChild(cards1)
+}
