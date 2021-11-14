46a731c73b9e4f2c723d7862eaf8390614485726
sebastianmiracastro <smira44@misena.edu.co>
2021-11-14 14:36:18 -0500

    Estilos Faltentes, Funcionalidades Hechas

--- Js10/app.js
+++ Js10/app.js
@@ -1,10 +1,14 @@
-const URLAll = 'https://rickandmortyapi.com/api/character/'
+const URL = 'https://rickandmortyapi.com/api/character/'
 const SelectContainer = document.getElementById('SelectContainer')
 const SeeCharacter = document.getElementById('AllCharacters')
-const character = document.getElementById('Character')
+const MainContainer = document.getElementById('MainContainer')
+
+SelectContainer.addEventListener('click', OptionCharacters)
+
+Character();
 
 function Character() {
-    fetch(URLAll).then(response => response.json()).then(data => {
+    fetch(URL).then(response => response.json()).then(data => {
 
         data.results.map((Element) => {
             const Name = document.createElement('option');
@@ -17,24 +21,40 @@ function Character() {
     .catch(error => console.log(error));
 }
 
-Character();
-
-SelectContainer.addEventListener('click', OptionCharacters)
-
 function OptionCharacters() {
     const ContainerSelect1 = SelectContainer.value;
-    fetch(URLAll).then((response) => response.json()).then((data) => {
+    fetch(URL).then((response) => response.json()).then((data) => {
         if (ContainerSelect1 === SeeCharacter.value) {
-            character.innerHTML= '';
-            data.results.map(element => {CreateCart(element)})
+            MainContainer.innerHTML= '';
+            data.results.map(e => {CreateCard(e)})
         }
         else {
-            data.results.map(element => {
-                if(element.name === ContainerSelect1){
-                    character.innerHTML = ''; CreateCart(element)
+            data.results.map(e => {
+                if(e.name === ContainerSelect1){
+                    MainContainer.innerHTML = ''; CreateCard(e)
                 }
             })
         }
     })
 }
 
+const data = []
+
+function CreateCard(data) {
+    const CardContainer = document.createElement('div')
+    const NameCard = document.createElement('h2')
+    const ImgCard = document.createElement('img')
+    const Gender = document.createElement('p')
+
+    Gender.textContent = data.gender;
+    NameCard.textContent = data.name;
+    ImgCard.setAttribute('id', data.id)
+    ImgCard.setAttribute('src', data.image)
+    ImgCard.setAttribute('alt', data.name)
+
+    CardContainer.appendChild(NameCard)
+    CardContainer.appendChild(ImgCard)
+    CardContainer.appendChild(Gender)
+
+    MainContainer.appendChild(CardContainer)
+}
