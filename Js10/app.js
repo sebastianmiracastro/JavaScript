ab97db6c4862f8c2aca6bfa7f51acb19723c827d
sebastianmiracastro <smira44@misena.edu.co>
2021-11-16 07:51:20 -0500

    Trabajo Listo Para Despliegue

--- Js10/app.js
+++ Js10/app.js
@@ -52,6 +52,22 @@ function CreateCard(data) {
     ImgCard.setAttribute('src', data.image)
     ImgCard.setAttribute('alt', data.name)
 
+    CardContainer.style.color = "#1C1C1C"
+    CardContainer.style.width = "17rem"
+    CardContainer.style.height = "26rem"
+    CardContainer.style.borderRadius = "15px"
+    CardContainer.style.display = "flex"
+    CardContainer.style.flexDirection = "column"
+    CardContainer.style.alignItems = "center"
+    CardContainer.style.margin = "0.5rem"
+    CardContainer.style.background = "#9B9B9B"
+    CardContainer.style.boxShadow = "0 0 20px #F4F4F4"
+
+    ImgCard.style.width = "90%"
+    ImgCard.style.height = "50%"
+    ImgCard.style.boxShadow = "0 0 20px #9B9B9B"
+    ImgCard.style.marginTop = "2rem"
+
     CardContainer.appendChild(NameCard)
     CardContainer.appendChild(ImgCard)
     CardContainer.appendChild(Gender)
